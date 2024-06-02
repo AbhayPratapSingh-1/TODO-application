59ee29a1d6cc2afc38d1a57c442f996ae25a0224
Abhay Pratap Singh <99877799+AbhayPratapSingh-1@users.noreply.github.com>
2024-06-02 07:43:32 +0530

    Create taskcontrollers.js

---
+++ taskcontrollers.js
@@ -0,0 +1,35 @@
+const Task = require('../models/task');
+
+const createTask = async (req, res) => {
+    try {
+        const newTask = new Task(req.body);
+        const savedTask = await newTask.save();
+        res.status(201).json(savedTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const updateTask = async (req, res) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = {
+    createTask,
+    getAllTasks,
+    updateTask
+};
