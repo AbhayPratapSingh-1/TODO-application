14d2a4c3b060e6a42c19f3974bca9e5cec3846a3
Abhay Pratap Singh <99877799+AbhayPratapSingh-1@users.noreply.github.com>
2024-06-02 07:44:04 +0530

    Create taskroutes.js

---
+++ taskroutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+router.post('/', taskController.createTask);
+router.get('/', taskController.getAllTasks);
+router.put('/:id', taskController.updateTask);
+
+module.exports = router;
